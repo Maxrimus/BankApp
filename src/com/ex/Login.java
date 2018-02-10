6477986abde0e083d9fe28ba8c192a276ad2f65f
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-10 15:38:26 -0500

    Login file ready to implement

--- src/com/ex/Login.java
+++ src/com/ex/Login.java
@@ -1,4 +1,20 @@
 package com.ex;
 
+import static com.ex.User.Admin;
+import static com.ex.User.Customer;
+import static com.ex.User.Employee;
+
 public class Login {
+    User user;
+
+    public Login(User user){
+        this.user = user;
+    }
+
+    public void login(String username, String password){
+
+    }
+
+
+
 }
