fda811bf7caac8c84da87cca06aeefceb24a5eb4
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-10 15:39:07 -0500

    Multiple validation testing methods created

--- src/com/ex/ValidFormat.java
+++ src/com/ex/ValidFormat.java
@@ -6,7 +6,126 @@ public class ValidFormat {
 
     }
 
-    public static boolean isMinMaxLength(String input, int min, int max){
+    public static String loopUntilValid(String input, String command){
+        //  Commands:
+        // LETTERS
+        // ALPHANUM
+
+        switch (command){
+            case "LETTERS":{
+                boolean invalid = true;
+                while (invalid){
+                    if (isAllLetters(input)){
+                        invalid = false;
+                    }
+                    else{
+                        System.out.println("Input is not matching\nPlease try again");
+                        input = Main.getUserInput();
+                    }
+                }
+
+                break;
+            }
+            case "ALPHANUM":{
+                boolean invalid = true;
+                while (invalid){
+                    if (isAlphanum(input)){
+                        invalid = false;
+                    }
+                    else{
+                        System.out.println("Input is not matching\nPlease try again");
+                        input = Main.getUserInput();
+                    }
+                }
+                break;
+            }
+        }
+
+        return input;
+    }
+
+    public static String loopUntilValid(String input, String command, int min, int max){
+        // Commands:
+        // LENGTH
+        // VALUE
+
+        switch (command){
+            case "LENGTH":{
+                boolean invalid = true;
+                while (invalid){
+                    if (isBetweenLength(input,min,max)){
+                        invalid = false;
+                    }
+                    else{
+                        System.out.println("Input is not matching\nPlease try again");
+                        input = Main.getUserInput();
+                    }
+                }
+                break;
+            }
+            case "VALUE":{
+                boolean invalid = true;
+                while (invalid){
+                    if (isAnIntBetween(input,min,max)){
+                        invalid = false;
+                    }
+                    else{
+                        System.out.println("Input is not matching\nPlease try again");
+                        input = Main.getUserInput();
+                    }
+                }
+                break;
+            }
+        }
+        return input;
+    }
+
+    public static boolean isBetweenLength(String input, int minLength, int maxLength){
+        int n = input.length();
+        if (n >= minLength && n <= maxLength){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean isAnIntBetween(String input, int minValue, int maxValue){
+        String specialchar = "/*!@#$%^&*()\"{}_[]|\\?/<>,.";
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                System.out.println("You did not enter a number");
+                return false;
+            }
+        }
+        Integer n = Integer.parseInt(input);
+        if (n >= minValue && n <= maxValue){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean isAllLetters(String input){
+        for (int i = 0; i < input.length(); i++){
+            if (!Character.isLetter(input.charAt(i))){
+                System.out.println("You entered number(s)");
+                return false;
+            }
+        }
         return true;
     }
+
+    public static boolean isAlphanum(String input){
+        for (int i = 0; i < input.length(); i++){
+            if (!Character.isLetterOrDigit(input.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 }
