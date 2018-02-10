461857ba61a3da13186f6871148211cb8aa2e933
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-10 17:18:26 -0500

    implemented writing and reading methods

--- src/com/ex/serialize/WriteData.java
+++ src/com/ex/serialize/WriteData.java
@@ -1,4 +1,46 @@
 package com.ex.serialize;
 
+import com.ex.accprofile.BankAccount;
+import com.ex.accprofile.BankProfile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class WriteData {
+    static ObjectOutputStream out = null;
+
+    public static void writeBankData(String username, BankAccount bankacc) {
+        try {
+            out = new ObjectOutputStream(new FileOutputStream("resources/bankaccounts"+username));
+            out.writeObject(bankacc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void writeProfileData(String username, BankProfile bankpro){
+        try {
+            out = new ObjectOutputStream(new FileOutputStream("resources/bankaccounts"+username));
+            out.writeObject(bankpro);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
