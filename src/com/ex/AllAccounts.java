72eeac7c4ac5d4d69be8802952355f82c628164c
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-10 16:34:14 -0500

    Added skeleton functions and fields

--- src/com/ex/AllAccounts.java
+++ src/com/ex/AllAccounts.java
@@ -1,4 +1,41 @@
 package com.ex;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+//This class is a Singleton
 public class AllAccounts {
+    private HashSet<String> customerUsernames = new HashSet<>();
+    private HashSet<String> employeeUsernames = new HashSet<>();
+    private HashSet<String> adminUsernames = new HashSet<>();
+
+    //Map Keys: password
+    //Map Values: username
+    private HashMap<String,String> customerPass = new HashMap<>();
+    private HashMap<String,String> employeePass = new HashMap<>();
+    private HashMap<String,String> adminPass = new HashMap<>();
+
+    private AllAccounts(){}
+
+    public static AllAccounts accounts = new AllAccounts();
+
+    public boolean doesUserExist(User user, String username){
+        return false;
+    }
+
+    public boolean doesPassMatch(User user, String username, String password){
+        return false;
+    }
+
+    public void newUsername(User user, String username){
+
+    }
+
+    public void createAccount(User user, String username, String password){
+
+    }
+
+    public void deleteAccount(User user, String username, String password){
+
+    }
 }
