b094f4a8549071b439e3e533eae58161e4bd0b31
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-12 17:25:35 -0500

    can now use register method. Still must implement account access

--- src/com/ex/makingaccount/Register.java
+++ src/com/ex/makingaccount/Register.java
@@ -1,19 +1,42 @@
 package com.ex.makingaccount;
 
+import com.ex.Main;
+import com.ex.ValidFormat;
+
 public class Register {
     private String username;
     private String password;
     private String email;
-    private String securityQ1;
-    private String securityQ2;
-    private String securityQ3;
-    private String securityA1;
-    private String securityA2;
-    private String securityA3;
+
+    //private String securityQ1;
+    //private String securityQ2;
+    //private String securityQ3;
+    //private String securityA1;
+    //private String securityA2;
+    //private String securityA3;
 
     public Register() {}
 
-    public void register(){}
+    public void register(){
+        System.out.println("Enter Username:");
+        String userInput = Main.getUserInput();
+        userInput = ValidFormat.loopUntilValid(userInput, "ALPHANUM");
+        userInput = ValidFormat.loopUntilValid(userInput, "SPACE");
+        userInput = ValidFormat.loopUntilValid(userInput, "EXIST");
+        username = userInput;
+
+        System.out.println("Enter Password");
+        userInput = ValidFormat.loopUntilValid(userInput, "SPACE");
+        userInput = ValidFormat.loopUntilValid(userInput, "PASSWORD");
+        password = userInput;
+
+        System.out.println("Enter your email");
+        userInput = ValidFormat.loopUntilValid(userInput, "SPACE");
+        userInput = ValidFormat.loopUntilValid(userInput, "EMAIL");
+        email = userInput;
+
+
+    }
 
     public void setUsername(String username) {
         this.username = username;
@@ -27,29 +50,29 @@ public class Register {
         this.email = email;
     }
 
-    public void setSecurityQ1(String securityQ1) {
-        this.securityQ1 = securityQ1;
-    }
-
-    public void setSecurityQ2(String securityQ2) {
-        this.securityQ2 = securityQ2;
-    }
-
-    public void setSecurityQ3(String securityQ3) {
-        this.securityQ3 = securityQ3;
-    }
-
-    public void setSecurityA1(String securityA1) {
-        this.securityA1 = securityA1;
-    }
-
-    public void setSecurityA2(String securityA2) {
-        this.securityA2 = securityA2;
-    }
-
-    public void setSecurityA3(String securityA3) {
-        this.securityA3 = securityA3;
-    }
+//    public void setSecurityQ1(String securityQ1) {
+//        this.securityQ1 = securityQ1;
+//    }
+//
+//    public void setSecurityQ2(String securityQ2) {
+//        this.securityQ2 = securityQ2;
+//    }
+//
+//    public void setSecurityQ3(String securityQ3) {
+//        this.securityQ3 = securityQ3;
+//    }
+//
+//    public void setSecurityA1(String securityA1) {
+//        this.securityA1 = securityA1;
+//    }
+//
+//    public void setSecurityA2(String securityA2) {
+//        this.securityA2 = securityA2;
+//    }
+//
+//    public void setSecurityA3(String securityA3) {
+//        this.securityA3 = securityA3;
+//    }
 
     @Override
     public String toString() {
@@ -57,12 +80,12 @@ public class Register {
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
-                ", securityQ1='" + securityQ1 + '\'' +
-                ", securityQ2='" + securityQ2 + '\'' +
-                ", securityQ3='" + securityQ3 + '\'' +
-                ", securityA1='" + securityA1 + '\'' +
-                ", securityA2='" + securityA2 + '\'' +
-                ", securityA3='" + securityA3 + '\'' +
+//                ", securityQ1='" + securityQ1 + '\'' +
+//                ", securityQ2='" + securityQ2 + '\'' +
+//                ", securityQ3='" + securityQ3 + '\'' +
+//                ", securityA1='" + securityA1 + '\'' +
+//                ", securityA2='" + securityA2 + '\'' +
+//                ", securityA3='" + securityA3 + '\'' +
                 '}';
     }
 }
