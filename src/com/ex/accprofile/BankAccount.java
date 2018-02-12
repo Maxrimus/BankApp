dcc4ec88387b5004010f67c821e7a363c8f384b0
Calvin Zheng <calvinzheng240@gmail.com>
2018-02-12 15:58:18 -0500

    BankAccount and BankProfile classes are now defined with fields

--- src/com/ex/accprofile/BankAccount.java
+++ src/com/ex/accprofile/BankAccount.java
@@ -1,4 +1,47 @@
 package com.ex.accprofile;
 
+import java.util.HashMap;
+
 public class BankAccount {
+    private String username;
+    private int numberOfAccounts;
+
+    // Key Integer values so far:
+    // 1 - Checking account balance
+    // 2 - Savings account balance
+    private HashMap<Integer, Double> accType_Balance = new HashMap<>();
+
+    public BankAccount(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public int getNumberOfAccounts() {
+        return numberOfAccounts;
+    }
+
+
+    public void setNumberOfAccounts(int numberOfAccounts) {
+        this.numberOfAccounts = numberOfAccounts;
+    }
+
+    public HashMap<Integer, Double> getAccType_Balance() {
+        return accType_Balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "username='" + username + '\'' +
+                ", numberOfAccounts=" + numberOfAccounts +
+                '}';
+    }
 }
--- src/com/ex/accprofile/BankProfile.java
+++ src/com/ex/accprofile/BankProfile.java
@@ -1,4 +1,55 @@
 package com.ex.accprofile;
 
 public class BankProfile {
+
+    private String username;
+    private String email;
+    private String ssn;
+    private String birthdate;
+
+    public BankProfile(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "BankProfile{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                '}';
+    }
 }
