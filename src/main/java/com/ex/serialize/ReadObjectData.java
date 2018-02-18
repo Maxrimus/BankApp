b702cfbd44620ecf0bdf4aa43d4b8517e439b43b
Maxrimus <joe.p.tursi@gmail.com>
2018-02-17 20:09:43 -0500

    Minor tweaks before merging

--- src/main/java/com/ex/serialize/ReadObjectData.java
+++ src/main/java/com/ex/serialize/ReadObjectData.java
@@ -12,6 +12,7 @@ import com.ex.makingaccount.AllApplications;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -35,8 +36,8 @@ public class ReadObjectData {
 
         System.out.println("Did not find login info file");
         return accounts;*/
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT username,password,accesslevel FROM profiles");
+        PreparedStatement statement = conn.prepareStatement("SELECT username,password,accesslevel FROM profiles");
+        ResultSet resultSet = statement.executeQuery();
         while(resultSet.next()){
             if(resultSet.getInt("accesslevel") == 0) accounts.getCustomerPass().put(resultSet.getString("username"),resultSet.getString("password"));
             if(resultSet.getInt("accesslevel") == 1) accounts.getEmployeePass().put(resultSet.getString("username"),resultSet.getString("password"));
@@ -45,12 +46,12 @@ public class ReadObjectData {
     }
 
     public static AllApplications loadApplications() throws SQLException {
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM applications");
+        PreparedStatement statement = conn.prepareStatement("SELECT username,email,accounttype,first_name1,last_name1,ssn1,employment1 FROM applications");
+        ResultSet resultSet = statement.executeQuery();
         while(resultSet.next()){
             AccountApplication accountApplication = new AccountApplication(resultSet.getString("username"),resultSet.getString("email"),resultSet.getInt("accounttype"),
-                    resultSet.getString("first_name")+" "+resultSet.getString("last_name"),resultSet.getString("ssn"),
-                    resultSet.getString("employment"));
+                    resultSet.getString("first_name1")+" "+resultSet.getString("last_name1"),resultSet.getString("ssn1"),
+                    resultSet.getString("employment1"));
             AllApplications.getApplications().enterNewApplication(accountApplication);
         }
         return AllApplications.getApplications();
@@ -65,11 +66,12 @@ public class ReadObjectData {
         }
 
         System.out.println("bankaccdata file currently empty. Creating new instance");*/
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM accounts");
-        Statement statement1 = conn.createStatement();
+        PreparedStatement statement = conn.prepareStatement("SELECT id,accountnumber,balance,accounttype FROM accounts");
+        ResultSet resultSet = statement.executeQuery();
         while(resultSet.next()){
-            ResultSet resultSet1 = statement1.executeQuery("SELECT username FROM profiles WHERE id=(SELECT profileid FROM accountsprofiles WHERE accountid = "+ resultSet.getInt("id") +")");
+            PreparedStatement statement1 = conn.prepareStatement("SELECT username FROM profiles WHERE id=(SELECT profileid FROM accountsprofiles WHERE accountid = ?)");
+            statement1.setInt(1, resultSet.getInt("id"));
+            ResultSet resultSet1 = statement1.executeQuery();
             while(resultSet1.next()){
                 BankAccount bankAccount = new BankAccount(resultSet1.getString("username"),resultSet.getString("accountnumber"),resultSet.getInt("balance")
                         ,resultSet.getInt("accounttype"));
@@ -87,8 +89,8 @@ public class ReadObjectData {
 //            //e.printStackTrace();
 //        }
 //        System.out.println("bankprodata file currently empty. Creating new instance");
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM profiles");
+        PreparedStatement statement = conn.prepareStatement("SELECT username,email,first_name,last_name,ssn,employment FROM profiles");
+        ResultSet resultSet = statement.executeQuery();
         Statement statement1 = conn.createStatement();
         while(resultSet.next()){
             BankProfile bankProfile = new BankProfile(resultSet.getString("username"),resultSet.getString("email"),
